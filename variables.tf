ceade06d1782f44c8a9d34e0982f21e90bffdb72
code-panther-ai <itspanther08@gmail.com>
2026-06-20 12:16:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,19 @@
-.
+variable "aws_region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "ssh_allowed_cidr" {
+  type    = string
+  default = "0.0.0.0/0"
+}
+
+variable "bucket_prefix" {
+  type    = string
+  default = "torque-ec2-s3-demo"
+}
